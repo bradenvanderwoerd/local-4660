0d9117d0e9bc4f01d42c246133a15f07ee1f636a
braegen2 <81393677+braegen2@users.noreply.github.com>
2026-02-02 11:39:39 -0800

    Initial lab 4

---
+++ lab4/sim/lab4_tb.sv
@@ -0,0 +1,7 @@
+// lab4_tb.sv
+// Description: Testbench for the lab4 module
+// Braden Vanderwoerd, 2026/02/02
+
+module lab4_tb;
+
+endmodule
---
+++ lab4/src/lab4.sv
@@ -0,0 +1,7 @@
+// lab4.sv
+// Description: Top-level module for the lab4 FPGA project
+// Braden Vanderwoerd, 2026/02/02
+
+module lab4 ;
+
+endmodule
